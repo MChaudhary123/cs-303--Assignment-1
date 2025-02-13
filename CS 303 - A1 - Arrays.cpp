3565bc40f15d93a29730faa52c603801d1c82a08
MChaudhary123 <157518019+MChaudhary123@users.noreply.github.com>
2025-02-13 13:59:55 -0600

    Update CS 303 - A1 - Arrays.cpp

--- CS 303 - A1 - Arrays.cpp
+++ CS 303 - A1 - Arrays.cpp
@@ -1,26 +1,27 @@
 // CS 303 - A1 - Arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Muhammad Chaudhary
 
-#include <iostream>
+
+ #include <iostream>
 #include <fstream>
 #include "functions.h"
 
 using namespace std;
 
 int main() {
-    int* arr = nullptr;
-    int size = 0;
+    int* arr = nullptr;  // Array pointer
+    int size = 0;        // Size of the array
 
-    // Read data from the input file into the array
+    // Read data from the input file
     ifstream inputFile("C:\\Users\\muham\\OneDrive\\Desktop\\A1input.txt");
-    if (!inputFile.is_open()) {
-        cerr << "Error opening file." << endl;
-        return 1;
+    if (!inputFile) {
+        cerr << "Error: Unable to open file." << endl;
+        return 1;  // Exit if file cannot be opened
     }
 
     int num;
     while (inputFile >> num) {
-        addValue(arr, size, num);
+        addValue(arr, size, num);  // Add numbers from file to array
     }
     inputFile.close();
 
@@ -34,17 +35,18 @@ int main() {
         cout << "Number " << searchNum << " not found." << endl;
     }
 
-    // Testing modifyValue with try-catch block
+    // Testing modifyValue function with try-catch
     try {
-        int modifyIndex = 5;
-        int newValue = 500;
+        int modifyIndex = 5;   // Index to modify
+        int newValue = 500;    // New value
         modifyValue(arr, size, modifyIndex, newValue);
+        cout << "Modified index " << modifyIndex << " to " << newValue << endl;
     }
     catch (const exception& e) {
         cout << "Error: " << e.what() << endl;
     }
 
-    // Testing addValue function
+    // Testing addValue function with try-catch
     try {
         int newInt = 101;
         addValue(arr, size, newInt);
@@ -54,9 +56,9 @@ int main() {
         cout << "Error: " << e.what() << endl;
     }
 
-    // Testing removeValue with try-catch block
+    // Testing removeValue function with try-catch
     try {
-        int removeIndex = 3;
+        int removeIndex = 3;  // Index to remove
         removeValue(arr, size, removeIndex);
         cout << "Removed value at index " << removeIndex << "." << endl;
     }
@@ -64,7 +66,7 @@ int main() {
         cout << "Error: " << e.what() << endl;
     }
 
-    // Clean up
+    // Free allocated memory
     delete[] arr;
 
     return 0;
